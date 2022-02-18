9f768527bd138f48f06c35e7581d9dd2c5b85b36
Your Name <you@example.com>
2022-02-18 18:18:04 +0800

    add exam-demo

--- action.js
+++ action.js
@@ -11,6 +11,10 @@ var defaults ={
         {
           user: "ray",
           text: "酷的文章"
+        },
+        {
+            user: "jjj",
+            text: "1234"
         }
       ]
 }
@@ -27,6 +31,10 @@ var data ={
         {
           user: "ray",
           text: "酷的文章"
+        },
+        {
+            user: "jjj",
+            text: "1234"
         }
       ]
 }
@@ -38,6 +46,18 @@ var app = new Vue({
         titleTxt : data.titleTxt || defaults.titleTxt,
         imgScr : data.imgScr || defaults.imgScr,
         comments : data.comments || defaults.comments,
+        counter : 0,
+    },
+    methods : {
+        add : function(eventdata) {
+            console.log(eventdata);
+            // this.counter += 1;
+        }
+    },
+    computed: {
+        fullname() {
+            return this.last + this.name;
+        }
     }
 })
 var app4 = new Vue({
@@ -50,14 +70,28 @@ var app4 = new Vue({
         ]
     }
 })
-var app2 = new Vue({
-     el :"#app-2" ,
-     data : {
-         items : [
-
-         ]
-     }
-    })
+var ex1 = new Vue({
+    el : "#exam-1",
+    data : {
+        counter:0,
+    }
+})
+// var app2 = new Vue({
+//      el :"#app-2" ,
+//      data : {
+//          items : [
+//             {item:"1",index:"1"},
+//             {item:"2",index:"2"},
+//             {item:"3",index:"3"},
+//          ],
+//      }
+//     })
+var app6 = new Vue({  //顯示與資料綁定
+    el : "#app-6" ,
+    data : {
+        message :"Hello Vue!"
+    }
+})
 var app_test = new Vue({
     el: "#app_test",
 })
